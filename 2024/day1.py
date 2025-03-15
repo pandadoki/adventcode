1fb5c943ac0631c9ccc70380b0c72fcb34fb285e
pandadoki <joshybot2000@gmail.com>
2025-03-15 14:19:24 -0700

    day 1 solve python

---
+++ 2024/day1.py
@@ -0,0 +1,23 @@
+flist = []
+with open('input.txt') as file:
+    lines = file.readlines()
+    for line in lines:
+        numbers = line.strip().split()
+        for num in numbers:
+            flist.append(int(num))
+rightSort = []
+leftSort = []
+
+for x in range(len(flist)):
+    if(x%2):
+        rightSort.append(flist[x])
+    else:
+        leftSort.append(flist[x])
+
+rightSort.sort()
+leftSort.sort()
+
+z=0
+for i in range(len(leftSort)):
+    z = z+(abs((leftSort[i] - rightSort[i])))
+print(z)
