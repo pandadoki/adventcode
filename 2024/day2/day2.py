740d4f2810ebdc39efbe9e913c2ed23566f8e971
Joshua Wright <panda@mac.mynetworksettings.com>
2025-03-20 23:55:21 -0700

    update input, find sort

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -1,18 +1,21 @@
 flist = []
+count = 0
 with open('2024/day2/input.txt') as file:
     lines = file.readlines()
     for line in lines:
-        flist.append(line.strip())
-
-x=0
-for i in range(len(flist)):
-    flist.sort()
-    print(flist)
-    for n in int(flist[i]):
-        int(flist[i][n])
-        if flist[i][n] < flist[i][n]-3 or flist[i][n] > flist[i][n]+3:
-            x = x+1
-        else: 
-            x-1
-
-print(x)
+        store = 0
+        print(line)
+        afterline = (line.strip().split())
+        store = int(afterline[0])
+        for x in afterline[1:]:
+            x = int(x)
+            print(x)
+            print(store)
+            if not ((store)+3 <= x <= (store)-3):
+                print("yippee")
+                store = x
+            else:
+                count = count+1
+                break
+print(count)
+            
