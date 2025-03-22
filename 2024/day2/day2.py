809c53c5b4caa5f0c691b1389d47871831c82127
pandadoki <joshybot2000@gmail.com>
2025-03-21 23:58:27 -0700

    working toward solution

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -7,15 +7,39 @@ with open('2024/day2/input.txt') as file:
         print(line)
         afterline = (line.strip().split())
         store = int(afterline[0])
-        for x in afterline[1:]:
-            x = int(x)
-            print(x)
-            print(store)
-            if not ((store)+3 <= x <= (store)-3):
-                print("yippee")
-                store = x
+        print(int(afterline[1]))
+        if (int(afterline[1]) - store < 0):
+            print("decreasing")
+            for x in afterline[1:]:
+                x = int(x)
+                print(x)
+                print(store)
+                if(x > store+3 and x - store < 0):
+                    print("unsafe")
+                    count = count+1
+                    break
+                else:
+                    store = x
             else:
                 count = count+1
                 break
+        elif (int(afterline[1]) - store > 0): 
+            print("increasing")
+            for c in afterline[1:]:
+                c = int(c)
+                print(c)
+                print(store)
+                if(c-3 > store and c - store > 0):
+                    print("unsafe")
+                    count = count+1
+                    break
+                else:
+                    store = c
+            else:
+                count = count+1
+                break
+            
+    print(f"safe: + {count}")
+    print("")
 print(count)
             
