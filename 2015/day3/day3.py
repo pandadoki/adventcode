aa5f0ab3e4bf19075072da18311f61dbe201251b
pandadoki <joshybot2000@gmail.com>
2025-05-25 01:55:07 -0700

    working close, need unique set of cord

--- 2015/day3/day3.py
+++ 2015/day3/day3.py
@@ -1,3 +1,43 @@
-with open('2015/day3/input.txt') as file:
-    for line in file:
-        print(line)
+def solvepart1():
+    track = tuple()
+    count = 0
+
+    with open('2015/day3/input.txt') as file:
+        for line in file:
+            for index, char in enumerate(line):
+                track = list(track)
+                if char == '^': 
+                    count = [0,1]
+                    track.append(count)
+                elif char =='<':
+                    count = [-1,0]
+                    track.append(count)
+                elif char == 'v':
+                    count = [0,-1]
+                    track.append(count)
+                elif char == '>':
+                    count == [1,0]
+                    track.append(count)
+                track = tuple(track)
+        print(track)
+        count = 0
+        xcord = 0
+        ycord = 0
+        for index in track:
+            for x,i in enumerate(index):
+                print(track[i])
+                ar = track[i]
+                for m, i in enumerate(ar):
+                    print('this is m: ' + str(m))
+                    print(" this is i: " + str(i))
+                    if m == 0:
+                        xcord = xcord + i
+                        print(xcord)
+                    elif m == 1:
+                        ycord = ycord + i
+                        print(ycord)
+                    count = count + 1
+            print('final x cord ' + str(xcord))
+            print('final y cord ' + str(ycord))
+
+solvepart1()
