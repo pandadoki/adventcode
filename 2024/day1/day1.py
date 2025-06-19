6000edd50909ce27a94faa09d85bff203feac459
pandadoki <joshybot2000@gmail.com>
2025-06-18 20:58:35 -0700

    solve part2 2024

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,15 +1,36 @@
-list_1 = []
-list_2 = []
-with open('2024/day1/input.txt') as file:
-    for line in file:
-        two_words = line.strip().split("   ")
-        list_1.append(int(two_words[0]))
-        list_2.append(int(two_words[1]))
+def solvepart1():
+    list_1 = []
+    list_2 = []
+    with open('2024/day1/input.txt') as file:
+        for line in file:
+            two_words = line.strip().split("   ")
+            list_1.append(int(two_words[0]))
+            list_2.append(int(two_words[1]))
 
-list_1.sort()
-list_2.sort()
+    list_1.sort()
+    list_2.sort()
 
-z=0
-for i in range(len(list_1)):
-    z = z+(abs((list_1[i] - list_2[i])))
-print(z)
+    z=0
+    for i in range(len(list_1)):
+        z = z + (abs((list_1[i] - list_2[i])))
+    print(z)
+
+def solvepart2():
+    list_1 = []
+    list_2 = []
+    with open('2024/day1/input.txt') as file:
+        for line in file:
+            two_words = line.strip().split("   ")
+            list_1.append(int(two_words[0]))
+            list_2.append(int(two_words[1]))
+    list_1.sort()
+    list_2.sort()
+    z=0
+    for i in range(len(list_1)):
+        similarityScore = list_2.count(list_1[i])
+        z = z + ((list_1[i] * similarityScore))
+    print(z)
+
+
+solvepart1()
+solvepart2()
