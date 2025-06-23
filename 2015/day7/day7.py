5dcbc6f57b58414d8d548e1aacf206994d78a5c7
pandadoki <joshybot2000@gmail.com>
2025-06-22 17:06:55 -0700

    plan fix circuits

--- 2015/day7/day7.py
+++ 2015/day7/day7.py
@@ -18,26 +18,29 @@ def solve1():
             parts = [part.strip() for part in line.split('->')]
             circuits[parts[-1]] = parts[0]
             circuits = dict(sorted(circuits.items()))
-        #print(circuits.items())
+        print(circuits.items())
         while circuits['a'] == 'lx':
-            for x in circuits.items():
-                updateCircuit(x)
-                if "AND" in x[-1]:
-                    #print(x)
-                    circuits[x[0]] = x[-1]
-                elif "OR" in x[-1]:
-                    circuits[x[0]] = x[-1]
-                elif "RSHIFT" in x[-1]:
-                    circuits[x[0]] = x[-1]
-                elif "LSHIFT" in x[-1]:
-                    circuits[x[0]] = x[-1]
-                elif "NOT" in x[-1]:
-                    parts = [part.strip() for part in x[-1].split('NOT')]
-                    print(parts)
-                    circuits[x[0]] = int((65536-1) - parts[-1])
-                else:
-                    circuits[x[0]] = x[-1]
-                    circuits['a'] = 'lsklhf'
+            try:
+                for x in circuits.items():
+                    updateCircuit(x)
+                    if "AND" in x[-1]:
+                        #print(x)
+                        circuits[x[0]] = x[-1]
+                    elif "OR" in x[-1]:
+                        circuits[x[0]] = x[-1]
+                    elif "RSHIFT" in x[-1]:
+                        circuits[x[0]] = x[-1]
+                    elif "LSHIFT" in x[-1]:
+                        circuits[x[0]] = x[-1]
+                    elif "NOT" in x[-1]:
+                        parts = [part.strip() for part in x[-1].split('NOT')]
+                        print(parts)
+                        circuits[x[0]] = int((65536-1) - int(parts[-1]))
+                    else:
+                        circuits[x[0]] = x[-1]
+                        circuits['a'] = 'lsklhf'
+            except ValueError:    
+                circuits[x[0]] = x[-1]
 
 solve1()
 
